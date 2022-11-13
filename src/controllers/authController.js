b6a50bfaafb38aba6d0d59a2f54add2591f207e3
Henrique Panserini <henrique.panserini@gmail.com>
2022-11-13 17:33:46 -0300

    node.js_estruturaAPI-V0.1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,12 +5,16 @@ const User = requiere('../models/user.js');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const {email} = req.body;
     try{
-        const user = await User.create(req.body);
-        return  res.send({User});
+        if(await User.findOne({email})){
+            return res.send(400).send({error: 'User already exist!' });
+        }
+            const user = await User.create(req.body);
+            return  res.send({User});
     }catch(err){
         return err.status(400).send({error: 'Registration failed!'});
     }
 })
 
-module.exports = app => app.use('');
+module.exports = app => app.use('/auth', router);
