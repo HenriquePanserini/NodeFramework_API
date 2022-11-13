738461cb3bd878f8b64930f27470cb076284026a
Henrique Panserini <henrique.panserini@gmail.com>
2022-11-13 10:12:09 -0300

    node.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,5 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.get('/', (req, res) => {
-    res.send("Conectado!")
-});
-
-
+require('./controllers/authController')(app);
 app.listen(3000);
